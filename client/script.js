5d2ed608e21718f35a23819c8a35a576c2894a45
Adoo2401 <abdullahrehmani230@gmail.com>
2023-01-06 20:05:01 +0500

    final

--- client/script.js
+++ client/script.js
@@ -94,7 +94,7 @@ const handleSubmit=async(e)=>{
 
   //fetching Data from server
 
-  const response=await fetch('http://localhost:5000',{
+  const response=await fetch('https://adoo-api-production.up.railway.app',{
     method:"POST",
     headers:{"Content-Type":'application/json'},
     body:JSON.stringify({prompt:data.get("prompt")})
